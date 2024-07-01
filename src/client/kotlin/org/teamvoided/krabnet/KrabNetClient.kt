52194e08d01fdf65be33ba5760016980a5a897f8
theendercore <theendercore@gmail.com>
2024-07-01 19:54:14 +0300

    rename

---
+++ src/client/kotlin/org/teamvoided/krabnet/KrabNetClient.kt
@@ -0,0 +1,10 @@
+package org.teamvoided.krabnet
+
+import org.teamvoided.krabnet.KrabNet.log
+
+@Suppress("unused")
+object KrabNetClient{
+    fun init() {
+        log.info("Hello from Client")
+    }
+}
--- src/main/kotlin/org/teamvoided/krabnet/KrabNet.kt
+++ src/main/kotlin/org/teamvoided/krabnet/KrabNet.kt
@@ -1,15 +1,15 @@
-package org.teamvoided.template
+package org.teamvoided.krabnet
 
 import net.minecraft.util.Identifier
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @Suppress("unused")
-object Template {
+object KrabNet {
     const val MODID = "krabnet"
 
     @JvmField
-    val log: Logger = LoggerFactory.getLogger(Template::class.simpleName)
+    val log: Logger = LoggerFactory.getLogger(KrabNet::class.simpleName)
 
     fun init() {
         log.info("Hello from Common")
