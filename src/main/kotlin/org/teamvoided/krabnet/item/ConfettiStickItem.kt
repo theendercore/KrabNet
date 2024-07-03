9d6db220923ebe34731d7872ed5e9523cc67451c
theendercore <theendercore@gmail.com>
2024-07-03 20:18:32 +0300

    - got the confetti stick to work wel enough

--- src/main/kotlin/org/teamvoided/krabnet/item/ConfettiStickItem.kt
+++ src/main/kotlin/org/teamvoided/krabnet/item/ConfettiStickItem.kt
@@ -3,31 +3,32 @@ package org.teamvoided.krabnet.item
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
-import net.minecraft.server.world.ServerWorld
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
 import org.teamvoided.krabnet.init.KNParticleTypes
+import org.teamvoided.krabnet.utils.addParticle
+import kotlin.math.min
 
 class ConfettiStickItem(settings: Settings) : Item(settings) {
 
-    override fun use(worldIn: World, player: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
-        if (!worldIn.isClient) {
-            val world = worldIn as ServerWorld
+    override fun use(world: World, player: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+        if (world.isClient) {
+            val dir = player.getRotationVec(0f)
+            val random = world.random
+            repeat(150) {
+                val newDir = dir
+                    .add(random.nextDouble() - 0.5, random.nextDouble() - 0.1, random.nextDouble() - 0.5)
+                    .normalize()
+                    .multiply(min(random.nextDouble() + 0.2, 1.3))
+                    .add(player.velocity)
 
-            world.spawnParticles(
-                KNParticleTypes.CONFETTI,
-                player.x,
-                player.y + player.height / 2,
-                player.z,
-                50,
-                0.0,
-                0.0,
-                0.0,
-                2.0
-            )
+                world.addParticle(KNParticleTypes.CONFETTI, player.pos.add(0.0, 1.5, 0.0), newDir)
+                world.playSoundFromEntity( player, SoundEvents.BLOCK_NOTE_BLOCK_HARP.value(), SoundCategory.PLAYERS, 1.0f, 1.0f)
+            }
         }
-
-        return super.use(worldIn, player, hand)
+        return super.use(world, player, hand)
     }
 }
--- src/main/kotlin/org/teamvoided/krabnet/utils/Helper.kt
+++ src/main/kotlin/org/teamvoided/krabnet/utils/Helper.kt
@@ -1,8 +1,14 @@
 package org.teamvoided.krabnet.utils
 
+import net.minecraft.particle.ParticleEffect
 import net.minecraft.registry.Holder
 import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.World
 
 fun <T> Registry<T>.register(id: Identifier, entry: T): T = Registry.register(this, id, entry)
 fun <T> Registry<T>.registerHolder(id: Identifier, entry: T): Holder<T> = Registry.registerHolder(this, id, entry)
+
+fun World.addParticle(particle: ParticleEffect, pos: Vec3d, velocity: Vec3d) =
+    this.addParticle(particle, pos.x, pos.y, pos.z, velocity.x, velocity.y, velocity.z)
