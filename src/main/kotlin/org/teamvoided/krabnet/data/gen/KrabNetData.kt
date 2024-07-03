9bd6b62b4e3049d992f1c91851422e385d1a1c3f
theendercore <theendercore@gmail.com>
2024-07-03 13:25:34 +0300

    - added EnLangProvider

--- src/main/kotlin/org/teamvoided/krabnet/data/gen/KrabNetData.kt
+++ src/main/kotlin/org/teamvoided/krabnet/data/gen/KrabNetData.kt
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.minecraft.registry.RegistrySetBuilder
 import org.teamvoided.krabnet.KrabNet.log
 import org.teamvoided.krabnet.data.gen.prioviders.ModelProvider
+import org.teamvoided.krabnet.data.gen.prioviders.EnLangProvider
 
 @Suppress("unused")
 class KrabNetData : DataGeneratorEntrypoint {
@@ -13,6 +14,7 @@ class KrabNetData : DataGeneratorEntrypoint {
         val pack = gen.createPack()
 
         pack.addProvider(::ModelProvider)
+        pack.addProvider(::EnLangProvider)
     }
 
     override fun buildRegistry(gen: RegistrySetBuilder) {
---
+++ src/main/kotlin/org/teamvoided/krabnet/data/gen/prioviders/EnLangProvider.kt
@@ -0,0 +1,26 @@
+package org.teamvoided.krabnet.data.gen.prioviders
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider
+import net.minecraft.item.Item
+import net.minecraft.registry.HolderLookup
+import net.minecraft.registry.Registries
+import net.minecraft.util.Identifier
+import org.apache.commons.lang3.text.WordUtils
+import org.teamvoided.krabnet.init.KNItems
+import org.teamvoided.krabnet.init.KNTabs
+import java.util.concurrent.CompletableFuture
+
+class EnLangProvider(o: FabricDataOutput, r: CompletableFuture<HolderLookup.Provider>) : FabricLanguageProvider(o, r) {
+    override fun generateTranslations(reg: HolderLookup.Provider, gen: TranslationBuilder) {
+        KNItems.tabItems.forEach {
+            gen.add(it.translationKey, lang(it.id))
+        }
+
+        KNTabs.KN_TAB.key.ifPresent { gen.add(it, lang(it.value)) }
+    }
+
+    private fun lang(item: Identifier): String = WordUtils.capitalize(item.path.replace("_", " "))
+
+    private val Item.id get() = Registries.ITEM.getId(this)
+}
