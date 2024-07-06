cda6cb4555dd4f04a13cd1460f5be8efb89333e5
theendercore <theendercore@gmail.com>
2024-07-06 18:09:24 +0300

    - bomb is now nice

--- src/main/kotlin/org/teamvoided/krabnet/entity/ConfettiBombEntity.kt
+++ src/main/kotlin/org/teamvoided/krabnet/entity/ConfettiBombEntity.kt
@@ -2,13 +2,14 @@ package org.teamvoided.krabnet.entity
 
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.projectile.ProjectileEntity
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity
 import net.minecraft.item.Item
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvents
 import net.minecraft.util.hit.BlockHitResult
-import net.minecraft.util.hit.HitResult
+import net.minecraft.util.hit.EntityHitResult
 import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
@@ -21,29 +22,27 @@ import kotlin.math.min
 
 class ConfettiBombEntity : ThrownItemEntity {
     constructor(entityType: EntityType<out ConfettiBombEntity>, world: World) : super(entityType, world)
-    constructor( world: World) : super(KNEntityTypes.CONFETTI_BOMB, world)
+    constructor(world: World) : super(KNEntityTypes.CONFETTI_BOMB, world)
     constructor(world: World, owner: LivingEntity) : super(KNEntityTypes.CONFETTI_BOMB, owner, world)
 
     override fun getDefaultItem(): Item = KNItems.CONFETTI_BOMB
 
-    override fun onCollision(hitResult: HitResult) {
-        super.onCollision(hitResult)
-        if (hitResult.type != HitResult.Type.BLOCK && !world.isClient) {
-            (world as ServerWorld).doExploding { randomVec() }
-            discard()
-        }
-    }
-
     override fun onBlockHit(hitResult: BlockHitResult) {
         super.onBlockHit(hitResult)
-        if (!world.isClient) {
-            (world as ServerWorld).doExploding { getVec(hitResult.side) }
-            discard()
-        }
+        if (world.isClient) return
+        (world as ServerWorld).doExploding { getVec(hitResult.side) }
+        discard()
+    }
+
+    override fun onEntityHit(hitResult: EntityHitResult) {
+        super.onEntityHit(hitResult)
+        if (hitResult.entity is ProjectileEntity || world.isClient) return
+        (world as ServerWorld).doExploding { randomVec() }
+        discard()
     }
 
     private fun ServerWorld.doExploding(vec: () -> Vec3d) {
-        this.playSound(pos, SoundEvents.ENTITY_GENERIC_EXPLODE.value(), SoundCategory.BLOCKS, 0.01f, 50.0f)
+        this.playSound(pos, SoundEvents.ENTITY_GENERIC_EXPLODE.value(), SoundCategory.BLOCKS, 0.04f, 50.0f)
         repeat(360) {
             val velocity = vec.invoke().normalize().multiply(min(random.nextDouble() + 0.2, 1.3))
             this.spawnParticles(KNParticleTypes.CONFETTI, pos, velocity)
