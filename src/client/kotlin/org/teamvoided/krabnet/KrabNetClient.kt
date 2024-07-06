281731af4e15adc886f32d2d844d95bdead318f3
theendercore <theendercore@gmail.com>
2024-07-06 15:14:24 +0300

    - forgor the entity renderer

--- src/client/kotlin/org/teamvoided/krabnet/KrabNetClient.kt
+++ src/client/kotlin/org/teamvoided/krabnet/KrabNetClient.kt
@@ -1,7 +1,10 @@
 package org.teamvoided.krabnet
 
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry
+import net.minecraft.client.render.entity.FlyingItemEntityRenderer
 import org.teamvoided.krabnet.KrabNet.log
+import org.teamvoided.krabnet.init.KNEntityTypes
 import org.teamvoided.krabnet.init.KNParticleTypes
 import org.teamvoided.krabnet.particle.ConfettiParticle
 
@@ -11,5 +14,7 @@ object KrabNetClient{
         log.info("Hello from Client")
         ParticleFactoryRegistry.getInstance().register(KNParticleTypes.CONFETTI, ConfettiParticle::ConfettiFactory)
         ParticleFactoryRegistry.getInstance().register(KNParticleTypes.CONFETTI_EMMITER, ConfettiParticle::ConfettiEmitterFactory)
+
+        EntityRendererRegistry.register(KNEntityTypes.CONFETTI_BOMB, ::FlyingItemEntityRenderer)
     }
 }
