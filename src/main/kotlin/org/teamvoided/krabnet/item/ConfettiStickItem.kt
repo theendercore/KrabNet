9ba38641d821e18406d39c2a421514cf6c75ca3c
theendercore <theendercore@gmail.com>
2024-07-06 18:08:20 +0300

    - confetti stick is now serverside only

--- src/main/kotlin/org/teamvoided/krabnet/item/ConfettiStickItem.kt
+++ src/main/kotlin/org/teamvoided/krabnet/item/ConfettiStickItem.kt
@@ -3,6 +3,7 @@ package org.teamvoided.krabnet.item
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvents
 import net.minecraft.stat.Stats
@@ -10,7 +11,8 @@ import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
 import org.teamvoided.krabnet.init.KNParticleTypes
-import org.teamvoided.krabnet.utils.addParticle
+import org.teamvoided.krabnet.utils.playSound
+import org.teamvoided.krabnet.utils.spawnParticles
 import kotlin.math.min
 
 class ConfettiStickItem(settings: Settings) : Item(settings) {
@@ -19,9 +21,10 @@ class ConfettiStickItem(settings: Settings) : Item(settings) {
         val stack = player.getStackInHand(hand)
 
         player.itemCooldownManager[this] = 2
-        if (world.isClient) {
+        if (!world.isClient) {
             val dir = player.getRotationVec(0f)
             val random = world.random
+            val serverW = world as ServerWorld
             repeat(150) {
                 val newDir = dir
                     .add(random.nextDouble() - 0.5, random.nextDouble() - 0.1, random.nextDouble() - 0.5)
@@ -29,11 +32,9 @@ class ConfettiStickItem(settings: Settings) : Item(settings) {
                     .multiply(min(random.nextDouble() + 0.2, 1.3))
                     .add(player.velocity)
 
-                world.addParticle(KNParticleTypes.CONFETTI, player.pos.add(0.0, 1.5, 0.0), newDir)
+                serverW.spawnParticles(KNParticleTypes.CONFETTI, player.pos.add(0.0, 1.5, 0.0), newDir)
             }
-            world.playSoundFromEntity(
-                player, SoundEvents.BLOCK_COBWEB_BREAK, SoundCategory.PLAYERS, 4.0f, 8.0f
-            )
+            serverW.playSound(player.pos, SoundEvents.BLOCK_COBWEB_BREAK, SoundCategory.PLAYERS, 4.0f, 8.0f)
         }
 
         player.incrementStat(Stats.USED.getOrCreateStat(this))
